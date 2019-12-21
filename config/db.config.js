d4f5a07de0968fdcdbe6e35926c1eb59515aa32c
Sergio Iturzaeta <gios@MacBook-Pro-de-Sergio.local>
2019-12-21 11:04:07 +0100

    wip

--- config/db.config.js
+++ config/db.config.js
@@ -1,5 +1,5 @@
 const mongoose = require ('mongoose')
-const MONGODB_URL = process.env.MONGO_URI
+const MONGODB_URL = 'mongodb://heroku_kqzx4slh:u2vcgqkf4o8ric570q7o3152mn@ds257668.mlab.com:57668/heroku_kqzx4slh'
 
 mongoose.connect(MONGODB_URL, {useNewUrlParser: true})
     .then(()=> console.info("Conection success"))
